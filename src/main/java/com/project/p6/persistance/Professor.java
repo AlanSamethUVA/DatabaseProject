c64c77ed0e49d614c5d72f20cc13cdf712b3cdc9
kcx44 <kendallcross44@gmail.com>
2024-12-02 20:11:08 -0500

    commit

--- src/main/java/com/project/p6/persistance/Professor.java
+++ src/main/java/com/project/p6/persistance/Professor.java
@@ -52,4 +52,4 @@ public class Professor {
         this.officeHours = officeHours;
     }
 }
-//comit
+//comitt
--- src/main/java/com/project/p6/persistance/ProfessorRepository.java
+++ src/main/java/com/project/p6/persistance/ProfessorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 
-
+//comit
 }
